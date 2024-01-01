141f7fd58bf2dd60e9c664667fc4d256abba782d
Precious <Preshomondi@gmail.com>
2024-01-01 23:21:59 +0300

    Create new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Bezier from "./components/Bezier";
+import Graph from "./components/Graph";
 
 
 function App() {
   return (
     <div className="App">
       <h1>I'm trying out bezier curves</h1>
+      <Graph/>
       <Bezier/>
     </div>
   );
---
+++ src/components/Graph.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useRef} from 'react'
+
+function Graph() {
+  return (
+    <div>Graph</div>
+  )
+}
+
+export default Graph
