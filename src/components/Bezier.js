c6873c7dc023ef898e1bc333966d1405c2a975f5
Precious <Preshomondi@gmail.com>
2024-01-01 22:45:55 +0300

    Canvas element and 2D rendering

---
+++ src/components/Bezier.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+
+function Bezier() {
+    useEffect(() => {
+        const canvas = document.getElementById('myCanvas')
+        const context = canvas.getContext('2d')
+
+    }, [])
+  return (
+    <div>Bezier</div>
+  )
+}
+
+export default Bezier
