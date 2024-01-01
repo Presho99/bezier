f90c73df34716b7dea295689c9d1aa199311636e
Precious <Preshomondi@gmail.com>
2024-01-01 23:31:08 +0300

    Create data points

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -8,7 +8,11 @@ function Graph() {
         const context = canvas.getContext('2d')
 
         const dataPoints = [
-            {}
+            {x:50, y:200},
+            {x:150, y:50},
+            {x:250, y:150},
+            {x:350, y:100},
+            {x:450, y:250}
         ]
     })
   return (
