3204bbeef493c09f74e2660ca876d348ef86bad3
Precious <Preshomondi@gmail.com>
2024-01-01 23:21:33 +0300

    Draw a curve

--- src/components/Bezier.js
+++ src/components/Bezier.js
@@ -5,9 +5,19 @@ function Bezier() {
         const canvas = document.getElementById('myCanvas')
         const context = canvas.getContext('2d')
 
+        // Draw a cubic bezier curve
+        context.beginPath()
+        context.moveTo(50, 250)
+        context.bezierCurveTo(150, 50, 350, 50, 450, 250)
+        context.lineWidth = 2
+        context.strokeStyle = 'blue'
+        context.stroke()
+
+
+
     }, [])
   return (
-    <div>Bezier</div>
+    <canvas id="myCanvas" width='500' height='500'></canvas>
   )
 }
 
