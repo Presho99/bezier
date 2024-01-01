dce093b18951e5079bbb39894563671f9268bc0d
Precious <Preshomondi@gmail.com>
2024-01-01 23:26:43 +0300

    Set up canvas element

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,6 +1,16 @@
 import React, {useEffect, useRef} from 'react'
 
 function Graph() {
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+
+        const dataPoints = [
+            {}
+        ]
+    })
   return (
     <div>Graph</div>
   )
